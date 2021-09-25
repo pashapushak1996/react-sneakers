f0628ffb61f31b772a6ad2270b723c7b4d096a25
pashapushak1996 <pashapushak1996@gmail.com>
2021-09-26 01:36:07 +0300

    add empty cart styles

--- src/components/drawer/index.jsx
+++ src/components/drawer/index.jsx
@@ -1,6 +1,19 @@
 import { CartItem } from '../cart-item';
 import styles from './Drawer.module.scss';
 
+const EmptyCart = () => {
+    return (
+        <div className={styles.emptyCart}>
+            <h3>Корзина пустая</h3>
+            <img className={styles.emptyCartImage} src="/img/emptyCart.svg" alt=""/>
+            <p>Добавте хоча б одну парку кросівок, щоб зробити заказ</p>
+            <button className="green_button">
+                <span>Вернутись назад</span>
+                <img src="/img/icons/arrow-left.svg" alt=""/>
+            </button>
+        </div>);
+};
+
 export const Drawer = (props) => {
     const {
         cartItems,
@@ -17,31 +30,35 @@ export const Drawer = (props) => {
                         <img src="/img/icons/exit.svg" alt=""/>
                     </div>
                 </div>
+                {!cartItems.length
+                    ? <EmptyCart/>
+                    : <div className={styles.cartItems}>
+                        <div>
+                            {cartItems.length ? cartItems.map((sneakers) => <CartItem
+                                onClickRemove={onClickRemove}
+                                key={sneakers.id}
+                                sneakers={sneakers}/>) : <div>Немає замовлень</div>}
+                        </div>
+                        <div>
+                            <div className={styles.drawer_priceBlock}>
+                                <ul>
+                                    <li>
+                                        <span>Итого: </span>
+                                        <b>{totalPrice} грн</b>
+                                    </li>
+                                    <li>
+                                        <span>Налог 5%: </span>
+                                        <b>{totalPrice * 0.05}</b>
+                                    </li>
+                                </ul>
+                            </div>
+                            <button className="green_button">
+                                <span>Оформить заказ</span>
+                                <img src="/img/icons/arrow-right.svg" alt=""/>
+                            </button>
+                        </div>
+                    </div>}
 
-                <div className="cartItems">
-                    {cartItems.length ? cartItems.map((sneakers) => <CartItem
-                        onClickRemove={onClickRemove}
-                        key={sneakers.id}
-                        sneakers={sneakers}/>) : <div>Немає замовлень</div>}
-                </div>
-                <div>
-                    <div className={styles.drawer_priceBlock}>
-                        <ul>
-                            <li>
-                                <span>Итого: </span>
-                                <b>{totalPrice} грн</b>
-                            </li>
-                            <li>
-                                <span>Налог 5%: </span>
-                                <b>{totalPrice * 0.05}</b>
-                            </li>
-                        </ul>
-                    </div>
-                    <button className="green_button">
-                        <span>Оформить заказ</span>
-                        <img src="/img/icons/arrow-right.svg" alt=""/>
-                    </button>
-                </div>
             </div>
         </div>
     );
